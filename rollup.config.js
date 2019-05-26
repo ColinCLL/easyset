ae4fcab2ed8e1b0776a72c5fbc34ed93bf8746de
陈立林 <245839124@qq.com>
2019-05-26 18:55:17 +0800

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,46 @@
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+
 export default {
   input: 'src/index.js',
-  format: 'umd',
-  dest: './build/bundle.js',
+  output: {
+    file: 'build/easycharts.js',
+    format: 'umd',
+    name: "ec"
+  },
+  namedExports: {
+    "jcalculator": ["jc"]
+  },
   plugins: [
-    resolve(),
+    resolve({
+      module: false
+    }),
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        'node_modules/jcalculator/JCalculator.min.js': ['jcalculator']
+      }
+    }),
     babel({
-      exclude: 'node_modules/**' // 仅仅转译我们的源码
+      exclude: 'node_modules/**' // 只编译我们的源代码
     })
-  ]
+  ],
+  // external: ["jcalculator"]
 };
+
+// export default {
+//   input: 'src/main.js',
+//   output: {
+//     file: 'bundle.js',
+//     format: 'umd',
+//     name: "easycharts.js"
+//   },
+//   plugins: [ resolve(), babel() ],
+//   // namedExports: {
+//   //   "jcalculator": "jc"
+//   // },
+//   // external: ["jcalculator"]
+// };
