0e04e69c23fcb4cf4822d84a914b3bb79d236fc4
陈立林 <245839124@qq.com>
2019-07-22 01:54:34 +0800

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "build/easycharts.js",
+    format: "umd",
+    name: "ec"
+  },
+  namedExports: {
+    jcalculator: ["jc"]
+  },
+  plugins: [
+    resolve({
+      module: false
+    }),
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        "node_modules/jcalculator/JCalculator.min.js": ["jcalculator"]
+      }
+    }),
+    babel({
+      exclude: "node_modules/**" // 只编译我们的源代码
+    })
+  ]
+};
