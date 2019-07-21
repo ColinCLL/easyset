63a91f3d55c5fa27d9b9d8e420187548837a4617
陈立林 <245839124@qq.com>
2019-07-22 02:08:21 +0800

    x1,0.0

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import babel from "rollup-plugin-babel";
 export default {
   input: "src/index.js",
   output: {
-    file: "build/easycharts.js",
+    file: "build/easycharts.min.js",
     format: "umd",
     name: "ec"
   },
