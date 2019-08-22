7963f00545175f37823b351ea6368bfce739ccf8
陈立林 <245839124@qq.com>
2019-08-23 03:41:50 +0800

    add adsense

---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <ParentLayout>
+    <adsense slot="sidebar-bottom" />
+    <!-- <AdSense slot="page-bottom" /> -->
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from "../../../node_modules/vuepress/lib/default-theme/Layout.vue";
+import adsense from "./components/adsense.vue";
+export default {
+  components: {
+    ParentLayout,
+    adsense
+  }
+};
+</script>
---
+++ docs/.vuepress/theme/components/adsense.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="adsense-content">
+    <!-- <div class="adsense-title">贊助商連結</div> -->
+    <div style="padding: 0 1.5rem;">
+      <Adsense data-ad-client="ca-pub-2747362703566068" data-ad-slot="1234567890"></Adsense>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.adsense-content {
+  max-width: 740px;
+  margin: 0 auto;
+}
+.adsense-title {
+  color: #999;
+  transition: color 0.15s ease;
+  cursor: pointer;
+  font-size: 1.1em;
+  font-weight: bold;
+  padding: 0 1.5rem;
+  margin-top: 0;
+  margin-bottom: 1rem;
+}
+</style>
